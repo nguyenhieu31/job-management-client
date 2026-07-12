23c25a7c4cb76f3f5d6700f7a2e160c14a523172
nguyenhieu31 <hieufaptv777@gmail.com>
2026-07-12 22:35:02 +0700

    feat(config): add Cloudinary remotePatterns for sample images

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
   async rewrites() {
     if (process.env.NEXT_PUBLIC_ENV === "development") {
       return [
